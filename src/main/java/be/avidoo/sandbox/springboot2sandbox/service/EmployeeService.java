daf1993eb1c700e1d7d2939fcc036f8b2cb4732f
marcobrasci1986 <marco_brasci@hotmail.com>
2018-06-29 22:25:11 +0200

    Add logging

--- src/main/java/be/avidoo/sandbox/springboot2sandbox/service/EmployeeService.java
+++ src/main/java/be/avidoo/sandbox/springboot2sandbox/service/EmployeeService.java
@@ -3,10 +3,12 @@ package be.avidoo.sandbox.springboot2sandbox.service;
 import be.avidoo.sandbox.springboot2sandbox.model.Employee;
 import be.avidoo.sandbox.springboot2sandbox.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
@@ -14,6 +16,10 @@ public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
     public List<Employee> findAll() {
+        log.debug("debug employee:: findAll");
+        log.info("info employee:: findAll");
+        log.warn("warn employee:: findAll");
+        log.error("error employee:: findAll");
         return this.employeeRepository.findAll();
     }
 
